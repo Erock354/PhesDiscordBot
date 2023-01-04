f96a4c544afa8b2021bc70cd65cf31cc4eda75a4
Edoardo Rocchio <edoardo.rocchio@marconirovereto.it>
2023-01-04 13:27:42 +0100

    v 1.0.8: Ho fatto i log e adesso la cartella delle foto generate sarà vuota. Ogni volta che verrà generata un immagine verrà eliminata alla fine del comando.

--- data_handler.py
+++ data_handler.py
@@ -1,5 +1,7 @@
 import os
 
+import discord
+
 
 def dir_init(guild_id):
     guild_path = os.path.join('assets/guilds/', f'{guild_id}')
@@ -13,3 +15,8 @@ def dir_init(guild_id):
     f = open(f"assets/guilds/{guild_id}/texts.txt", "a")
     f.write('balls\n')
     f.close()
+
+
+def log(command_name: str, member: discord.Member, guild_id: int, time: str):
+    print(f'Command {command_name} invoked by {member.name}#{member.discriminator} from guild <{guild_id}> has '
+          f'finished his execution at {time}.')
